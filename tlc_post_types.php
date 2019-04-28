aae1afe8dc51b8502d3ff2e57d1a71535c9c4ba4
Jon Howard <jon.showard@gmail.com>
2019-04-28 13:42:50 -0300

    reformat code

--- tlc_post_types.php
+++ tlc_post_types.php
@@ -15,205 +15,205 @@ Author URI:
 class TLC_Post_Types
 {
 
-    /**
-     * @constructor
-     */
-    public function TLC_Post_Types()
-    {
-        $this->set_hooks();
-    }
+	/**
+	 * @constructor
+	 */
+	public function TLC_Post_Types()
+	{
+		$this->set_hooks();
+	}
 
-    /**
-     * Set actions and filters
-     * @return void
-     */
-    public function set_hooks()
-    {
-        add_action('init', 'register_tlc_post_types');
-        add_action('generate_rewrite_rules', 'register_tlc_rewrite_rules');
-    }
+	/**
+	 * Set actions and filters
+	 * @return void
+	 */
+	public function set_hooks()
+	{
+		add_action('init', 'register_tlc_post_types');
+		add_action('generate_rewrite_rules', 'register_tlc_rewrite_rules');
+	}
 
-    /**
-     * A sample action
-     * @return WP_Query
-     */
-    // function parse_request( $wp ) {
-    //     return $wp;
-    // }
+	/**
+	 * A sample action
+	 * @return WP_Query
+	 */
+	// function parse_request( $wp ) {
+	//     return $wp;
+	// }
 
 }
 
 function register_tlc_post_types()
 {
-    register_taxonomy('service-category', ['services'],
-        [
-            'labels' => [
-                'name' => __('Service Categories'),
-                'menu_name' => __('Service Categories'),
-                'singular_name' => __('Service Category'),
-                'all_items' => __('All Categories'),
-            ],
-            'public' => true,
-            'hierarchical' => true,
-            'show_ui' => true,
-            'rewrite' => [
-                'slug' => 'service-category',
-                'hierarchical' => true,
-                'with_front' => false,
-            ],
-        ]
-    );
+	register_taxonomy('service-category', ['services'],
+		[
+			'labels' => [
+				'name' => __('Service Categories'),
+				'menu_name' => __('Service Categories'),
+				'singular_name' => __('Service Category'),
+				'all_items' => __('All Categories'),
+			],
+			'public' => true,
+			'hierarchical' => true,
+			'show_ui' => true,
+			'rewrite' => [
+				'slug' => 'service-category',
+				'hierarchical' => true,
+				'with_front' => false,
+			],
+		]
+	);
 
-    register_post_type('services',
-        [
-            'labels' => [
-                'name' => __('Services'),
-                'menu_name' => __('Service Manager'),
-                'singular_name' => __('Service'),
-                'all_items' => __('All Services'),
-                'add_new_item' => __('Add New Service'),
-            ],
-            'public' => true,
-            'publicly_queryable' => true,
-            'show_ui' => true,
-            'show_in_menu' => true,
-            'show_in_nav_menus' => true,
-            'supports' => [
-                'title', 
-                'editor', 
-                'excerpt', 
-                'thumbnail', 
-                'post-formats', 
-                'revisions', 
-                'page-attributes'
-            ],
-            'hierarchical' => false,
-            'has_archive' => true,
-            'taxonomies' => ['service-category'],
-            'rewrite' => [
-                'slug' => 'service',
-                'hierarchical' => true,
-                'with_front' => false,
-            ],
-        ]
-    );
+	register_post_type('services',
+		[
+			'labels' => [
+				'name' => __('Services'),
+				'menu_name' => __('Service Manager'),
+				'singular_name' => __('Service'),
+				'all_items' => __('All Services'),
+				'add_new_item' => __('Add New Service'),
+			],
+			'public' => true,
+			'publicly_queryable' => true,
+			'show_ui' => true,
+			'show_in_menu' => true,
+			'show_in_nav_menus' => true,
+			'supports' => [
+				'title',
+				'editor',
+				'excerpt',
+				'thumbnail',
+				'post-formats',
+				'revisions',
+				'page-attributes'
+			],
+			'hierarchical' => false,
+			'has_archive' => true,
+			'taxonomies' => ['service-category'],
+			'rewrite' => [
+				'slug' => 'service',
+				'hierarchical' => true,
+				'with_front' => false,
+			],
+		]
+	);
 
-    register_post_type('events',
-        [
-            'labels' => [
-                'name' => __('Events'),
-                'menu_name' => __('Event Manager'),
-                'singular_name' => __('Event'),
-                'all_items' => __('All Events'),
-                'add_new_item' => __('Add New Event'),
-            ],
-            'public' => true,
-            'publicly_queryable' => true,
-            'show_ui' => true,
-            'show_in_menu' => true,
-            'show_in_nav_menus' => true,
-            'supports' => [
-                'title', 
-                'editor', 
-                'thumbnail', 
-                'post-formats', 
-                'revisions'
-            ],
-            'hierarchical' => false,
-            'has_archive' => true,
-            'rewrite' => [
-                'slug' => 'event',
-                'hierarchical' => true,
-                'with_front' => false,
-            ],
-        ]
-    );
+	register_post_type('events',
+		[
+			'labels' => [
+				'name' => __('Events'),
+				'menu_name' => __('Event Manager'),
+				'singular_name' => __('Event'),
+				'all_items' => __('All Events'),
+				'add_new_item' => __('Add New Event'),
+			],
+			'public' => true,
+			'publicly_queryable' => true,
+			'show_ui' => true,
+			'show_in_menu' => true,
+			'show_in_nav_menus' => true,
+			'supports' => [
+				'title',
+				'editor',
+				'thumbnail',
+				'post-formats',
+				'revisions'
+			],
+			'hierarchical' => false,
+			'has_archive' => true,
+			'rewrite' => [
+				'slug' => 'event',
+				'hierarchical' => true,
+				'with_front' => false,
+			],
+		]
+	);
 
-    register_post_type('affiliations',
-        [
-            'labels' => [
-                'name' => __('Affiliations'),
-                'menu_name' => __('Affiliation Manager'),
-                'singular_name' => __('Affiliation'),
-                'all_items' => __('All Affiliations'),
-                'add_new_item' => __('Add New Affiliation'),
-            ],
-            'public' => false,
-            'publicly_queryable' => true,
-            'show_ui' => true,
-            'show_in_menu' => true,
-            'show_in_nav_menus' => false,
-            'supports' => [
-                'title', 
-                'thumbnail', 
-                'post-formats', 
-                'revisions', 
-                'page-attributes'
-            ],
-            'hierarchical' => false,
-            'has_archive' => false,
-        ]
-    );
+	register_post_type('affiliations',
+		[
+			'labels' => [
+				'name' => __('Affiliations'),
+				'menu_name' => __('Affiliation Manager'),
+				'singular_name' => __('Affiliation'),
+				'all_items' => __('All Affiliations'),
+				'add_new_item' => __('Add New Affiliation'),
+			],
+			'public' => false,
+			'publicly_queryable' => true,
+			'show_ui' => true,
+			'show_in_menu' => true,
+			'show_in_nav_menus' => false,
+			'supports' => [
+				'title',
+				'thumbnail',
+				'post-formats',
+				'revisions',
+				'page-attributes'
+			],
+			'hierarchical' => false,
+			'has_archive' => false,
+		]
+	);
 
-    register_post_type('featured-sections',
-        [
-            'labels' => [
-                'name' => __('Featured Sections'),
-                'menu_name' => __('Home Page Manager'),
-                'singular_name' => __('Featured Section'),
-                'all_items' => __('All Featured Sections'),
-                'add_new_item' => __('Add New Featured Section'),
-            ],
-            'public' => false,
-            'publicly_queryable' => true,
-            'show_ui' => true,
-            'show_in_menu' => true,
-            'menu_position' => 20,
-            'show_in_nav_menus' => false,
-            'supports' => [
-                'title', 
-                'revisions', 
-            ],
-            'hierarchical' => false,
-            'has_archive' => false,
-        ]
-    );
+	register_post_type('featured-sections',
+		[
+			'labels' => [
+				'name' => __('Featured Sections'),
+				'menu_name' => __('Home Page Manager'),
+				'singular_name' => __('Featured Section'),
+				'all_items' => __('All Featured Sections'),
+				'add_new_item' => __('Add New Featured Section'),
+			],
+			'public' => false,
+			'publicly_queryable' => true,
+			'show_ui' => true,
+			'show_in_menu' => true,
+			'menu_position' => 20,
+			'show_in_nav_menus' => false,
+			'supports' => [
+				'title',
+				'revisions',
+			],
+			'hierarchical' => false,
+			'has_archive' => false,
+		]
+	);
 
-    register_post_type('home-banner',
-        [
-            'labels' => [
-                'name' => __('Home Banners'),
-                'menu_name' => __('Banner Manager'),
-                'singular_name' => __('Home Banner'),
-                'all_items' => __('All Banners'),
-                'add_new_item' => __('Add New Banner'),
-            ],
-            'public' => false,
-            'publicly_queryable' => true,
-            'show_ui' => true,
-            'show_in_menu' => true,
-            'menu_position' => 20,
-            'show_in_nav_menus' => false,
-            'supports' => [
-                'title', 
-                'thumbnail',
-                'revisions', 
-            ],
-            'hierarchical' => false,
-            'has_archive' => false,
-        ]
-    );
+	register_post_type('home-banner',
+		[
+			'labels' => [
+				'name' => __('Home Banners'),
+				'menu_name' => __('Banner Manager'),
+				'singular_name' => __('Home Banner'),
+				'all_items' => __('All Banners'),
+				'add_new_item' => __('Add New Banner'),
+			],
+			'public' => false,
+			'publicly_queryable' => true,
+			'show_ui' => true,
+			'show_in_menu' => true,
+			'menu_position' => 20,
+			'show_in_nav_menus' => false,
+			'supports' => [
+				'title',
+				'thumbnail',
+				'revisions',
+			],
+			'hierarchical' => false,
+			'has_archive' => false,
+		]
+	);
 }
 
 function register_tlc_rewrite_rules($wp_rewrite)
 {
-    $tlc_rules = [
-        'service/([^/]+)/?$' => 'index.php?post_type=services&services=' . $wp_rewrite->preg_index(1),
-        'service-category/([^/]+)/?$' => 'index.php?service-category=' . $wp_rewrite->preg_index(1),
-        'event/([^/]+)/?$' => 'index.php?post_type=events&events=' . $wp_rewrite->preg_index(1),
-        'event-category/([^/]+)/?$' => 'index.php?event-category=' . $wp_rewrite->preg_index(1),
-    ];
-    $wp_rewrite->rules = $tlc_rules + $wp_rewrite->rules;
+	$tlc_rules = [
+		'service/([^/]+)/?$' => 'index.php?post_type=services&services=' . $wp_rewrite->preg_index(1),
+		'service-category/([^/]+)/?$' => 'index.php?service-category=' . $wp_rewrite->preg_index(1),
+		'event/([^/]+)/?$' => 'index.php?post_type=events&events=' . $wp_rewrite->preg_index(1),
+		'event-category/([^/]+)/?$' => 'index.php?event-category=' . $wp_rewrite->preg_index(1),
+	];
+	$wp_rewrite->rules = $tlc_rules + $wp_rewrite->rules;
 }
 
 $TLC_Post_Types = new TLC_Post_Types();
