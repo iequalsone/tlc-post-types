97490f46a92631bf87b728eb3a10321e36d460f4
Jon Howard <jon.showard@gmail.com>
2021-02-19 06:44:32 -0330

    update slug

--- tlc_post_types.php
+++ tlc_post_types.php
@@ -130,7 +130,7 @@ function register_tlc_post_types()
 			'has_archive' => true,
 			'rest_base' => 'events',
 			'rewrite' => [
-				'slug' => 'schedule',
+				'slug' => 'event-listings',
 				'hierarchical' => true,
 				'with_front' => false,
 			],
